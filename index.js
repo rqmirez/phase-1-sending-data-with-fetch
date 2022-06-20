aab664e23e0ff842264aab6718d7b92fdc6137a5
Judith Ramirez <rqmirez@gmail.com>
2022-06-20 15:38:22 -0400

    commit tree e76c8986853d468bcf1b1a43288eca0f809ef8c4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const body = document.getElementsByTagName("body")[0]
+
+function submitData(firstName, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    }
+    return fetch ("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(response => alert('Unauthorized Access'))
+}
+
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID
+    document.body.append(message)
+}
+submitData("Name", "name@name.com")
